d5d8f122c6fe12055b1c3da224f9e56b61aaa80c
Theinterna <yalvin58@gmail.com>
2024-07-21 14:55:28 -0700

    Allow inspector changes for gravity and jump force

--- Unit-3-Sound-and-Effects/Assets/Scripts/PlayerController.cs
+++ Unit-3-Sound-and-Effects/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRB;
+    public float jumpForce = 10.0f;
+    public float gravityModifer = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
-        
+        Physics.gravity *= gravityModifer;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            playerRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
