7cff22690d80d471d385af6cb0f694fd80f35dae
Theinterna <yalvin58@gmail.com>
2024-07-21 15:00:41 -0700

    Prevent double jumping

--- Unit-3-Sound-and-Effects/Assets/Scripts/PlayerController.cs
+++ Unit-3-Sound-and-Effects/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRB;
     public float jumpForce = 10.0f;
     public float gravityModifer = 1.0f;
+    private bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        isOnGround = true;
     }
 }
