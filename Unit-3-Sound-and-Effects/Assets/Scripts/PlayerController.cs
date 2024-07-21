7c80c19aefffc6fca694987bb935d73fef4fe547
Theinterna <yalvin58@gmail.com>
2024-07-21 14:51:06 -0700

    Add jumping functionality with spacebar

--- Unit-3-Sound-and-Effects/Assets/Scripts/PlayerController.cs
+++ Unit-3-Sound-and-Effects/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRB;
     // Start is called before the first frame update
     void Start()
     {
+        playerRB = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            playerRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
     }
 }
