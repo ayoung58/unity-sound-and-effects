9627b97b6ca2a890bbd31b775592ba2fc7824ee7
Theinterna <yalvin58@gmail.com>
2024-07-22 00:01:39 -0700

    Add dirt effect when running

--- Unit-3-Sound-and-Effects/Assets/Scripts/PlayerController.cs
+++ Unit-3-Sound-and-Effects/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRB;
     public ParticleSystem particleExplosion;
+    public ParticleSystem particleDirt;
     public float jumpForce = 10.0f;
     public float gravityModifer = 1.0f;
     private bool isOnGround = true;
@@ -27,18 +28,23 @@ public class PlayerController : MonoBehaviour
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            particleDirt.Stop();
         }
     }
 
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Ground")) {
             isOnGround = true;
+            if (!gameOver) {
+                particleDirt.Play();
+            }
         } else if (collision.gameObject.CompareTag("Obstacle")) {
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             particleExplosion.Play();
+            particleDirt.Stop();
         }
     }
 }
