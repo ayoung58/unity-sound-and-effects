00b883e465cdfe4f0009c293f697bdcb4c57b1f4
Theinterna <yalvin58@gmail.com>
2024-07-21 18:53:13 -0700

    Destroy obstacles after a certain distance

--- Unit-3-Sound-and-Effects/Assets/Scripts/MoveLeft.cs
+++ Unit-3-Sound-and-Effects/Assets/Scripts/MoveLeft.cs
@@ -6,6 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     public float speed = 10.0f;
     private PlayerController playerControllerScript;
+    private float leftBound = -15;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,10 @@ public class MoveLeft : MonoBehaviour
     {
         if (playerControllerScript.gameOver == false) {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle")) {
+            Destroy(gameObject);
         }
     }
 }
